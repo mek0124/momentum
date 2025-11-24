7951dad01a01fef394b7632eec34815a9028b9da
mek0124 <mek0124@proton.me>
2025-11-24 02:22:21 -0600

    created base styles & initialized tailwind

---
+++ app/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0C2B4E",
+        secondary: "#1A3D64",
+        accent: "#1D546C",
+        accentHover: "#F4F4F4",
+        foreground: "#a9a9a9",
+      },
+    },
+  },
+  plugins: [],
+}
