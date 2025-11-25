13d30e8bfe2c564b73f796ff5bdac2e6302d379b
mek0124 <mek0124@proton.me>
2025-11-25 00:57:56 -0600

    updated content path

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
@@ -16,3 +16,4 @@ export default {
   },
   plugins: [],
 }
+
