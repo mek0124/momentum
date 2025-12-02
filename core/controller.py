a489fe57bfaa1374beb2f7bbbd4a65a258a2d991
mek0124 <mek0124@proton.me>
2025-12-01 23:31:47 -0600

    added storage controller setter function

--- core/controller.py
+++ core/controller.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# add project root (parent of core/) so sibling package `storage` is importable
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
 from storage.controller import StorageController
 
 from typing import Tuple, Union
@@ -15,11 +21,32 @@ class CoreController:
         if not controller_found:
             return False, found_controller
         
-        return found_controller
-    
-    
-
+        return True, found_controller
 
 
 if __name__ == '__main__':
     core_controller = CoreController()
+    found_controller, response = core_controller.set_storage_controller()
+
+    from datetime import datetime
+    from uuid import uuid4
+
+    if not found_controller:
+        raise Exception(response)
+    
+    new_task = {
+        "id": str(uuid4()),
+        "title": "An example title6546084",
+        "details": "some exampe details464565",
+        "priority": 1,
+        "created_at": datetime.now(),
+        "updated_at": datetime.now(),
+        "is_completed": False
+    }
+
+    did_save, response = response.update_task(new_task)
+
+    if not did_save:
+        raise Exception(response)
+    
+    print(response)
