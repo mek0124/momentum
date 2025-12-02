f098e22f32566a7b3b1bb457f794e91457d968e1
mek0124 <mek0124@proton.me>
2025-12-02 12:42:11 -0600

    added commands to cli group

--- tm_cli/tm_cli.py
+++ tm_cli/tm_cli.py
@@ -1,5 +1,9 @@
-import click
 from commands.add import add_task
+from commands.edit import edit_task
+from commands.delete import delete_task
+from commands.list import list_tasks
+
+import click
 
 
 @click.group()
@@ -8,6 +12,9 @@ def tm_cli():
 
 
 tm_cli.add_command(add_task)
+tm_cli.add_command(edit_task)
+tm_cli.add_command(delete_task)
+tm_cli.add_command(list_tasks)
 
 
 if __name__ == '__main__':
