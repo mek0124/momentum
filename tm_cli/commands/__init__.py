6e7aae07ea5cbe391d8da7866d0599b41bb476f6
mek0124 <mek0124@proton.me>
2025-12-02 12:42:19 -0600

    added command import/export

--- tm_cli/commands/__init__.py
+++ tm_cli/commands/__init__.py
@@ -1,4 +1,7 @@
 from .add import add_task
+from .edit import edit_task
+from .delete import delete_task
+from .list import list_tasks
 
 
-__all__ = ["add_task"]
+__all__ = ["add_task", "edit_task", "delete_task", "list_tasks"]
