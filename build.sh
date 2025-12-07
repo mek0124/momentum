bda21224858a5b17f2c26ccafea88e6e739d08bb
mek0124 <mek0124@proton.me>
2025-12-07 11:38:16 -0600

    updated build script

--- build.sh
+++ build.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
 
-# all echo's and sleeps can be removed
-# these are fodder fill to give a bit
-# of realism to the build process instead
-# of instantaniety
+# Task Manager Build Script
 
 clear
 
-echo -e "Building Project... Please Wait..."
-
-sleep 2
-
 if [ -d ./dist ]; then
-  echo -e "\ndist directory detected"
-  echo -e "Removing dist directory"
   rm -rf ./dist
-  echo -e "dist directory removed"
 fi
 
 if [ -d ./build ]; then
-  echo -e "\nbuild directory detected"
-  echo -e "Removing build directory"
   rm -rf ./build
-  echo -e "build directory removed"
 fi
 
-sleep 1
-
-echo -e "\nBuilding project using: 'python3 -m build'... Please Wait... \n"
-
-sleep 2
-
-pyinstaller --onefile --windowed --add-data "app/data:app/data" main.py
-
-echo -e "\nBuild completed... Press enter to exit.."
-read
-exit
+python3 -m build
